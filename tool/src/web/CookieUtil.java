913e0e9e78011a5e83604865ba7f50204569f5cb
benqczyo <benqczyo@outlook.com>
2016-04-05 14:48:48 +0800

    On branch dev  Changes to be committed: 	new file:   tool/.classpath 	new file:   tool/.project 	new file:   tool/.settings/org.eclipse.jdt.core.prefs 	new file:   tool/src/web/CookieUtil.java

---
+++ tool/src/web/CookieUtil.java
@@ -0,0 +1,24 @@
+package web;
+
+public class CookieUtil {
+	
+	public class Cookie {
+		String path;
+		String name;
+		String value;
+		String time;
+	}
+	
+	public static void create(Cookie[] cookies) {
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				
+			}
+		}
+	}
+	
+	public static void delete(Cookie[] cookies) {
+		
+	}
+	
+}
