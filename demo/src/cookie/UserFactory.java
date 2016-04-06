9deff54492bd340394cb417512a0c4171667c597
benqczyo <benqczyo@outlook.com>
2016-04-06 09:53:04 +0800

    UPDATE demo

---
+++ demo/src/cookie/UserFactory.java
@@ -0,0 +1,25 @@
+package cookie;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class UserFactory {
+	
+	public static User create(Map<String, String> items) throws Exception {
+		User result = new User();
+		for (Entry<String, String> item : items.entrySet()) {
+			try {
+				PropertyDescriptor dp = new PropertyDescriptor(item.getKey(), User.class);
+				dp.getWriteMethod().invoke(result, item.getValue());
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			}
+			
+		}
+		return result;
+	}
+	
+}
