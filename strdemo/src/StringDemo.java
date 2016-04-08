4016df491aca2ce57d8c7bfc3b92f3ed33e28c0a
benqczyo <benqczyo@outlook.com>
2016-04-08 12:11:10 +0800

    CREATE strdemo

---
+++ strdemo/src/StringDemo.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String cmd = "4-3-2-1";
+		LinkedList<String> someList = new LinkedList<String>(Arrays.asList(cmd.split("-")));
+		someList.push("5");
+		if (someList.size() > 4)
+			someList.removeLast();
+		Object[] someArray = someList.toArray();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < someArray.length; i++) {
+			if (i > 0)
+				sb.append("-");
+			sb.append(someArray[i]);
+		}
+		System.out.println(sb.toString());
+	}
+}
