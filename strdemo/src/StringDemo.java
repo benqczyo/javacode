4530cf902d7adb67c6981e5e9870a5fe82499ef5
benqczyo <benqczyo@outlook.com>
2016-04-08 12:19:49 +0800

    UPDATE strdemo

--- strdemo/src/StringDemo.java
+++ strdemo/src/StringDemo.java
@@ -8,13 +8,14 @@ public class StringDemo {
 		String cmd = "4-3-2-1";
 		LinkedList<String> someList = new LinkedList<String>(Arrays.asList(cmd.split("-")));
 		someList.push("5");
-		if (someList.size() > 4)
-			someList.removeLast();
+		someList.push("6");
+		someList.push("7");
+		someList.push("8");
+		if (someList.size() > 4) someList.removeLast();
 		Object[] someArray = someList.toArray();
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < someArray.length; i++) {
-			if (i > 0)
-				sb.append("-");
+			if (i > 0) sb.append("-");
 			sb.append(someArray[i]);
 		}
 		System.out.println(sb.toString());
