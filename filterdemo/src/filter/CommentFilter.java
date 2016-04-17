ff9877c349ccf3373714e41001733a4f03d0f2e9
benqczyo <benqczyo@outlook.com>
2016-04-17 19:18:34 +0800

    UPDATE filter

--- filterdemo/src/filter/CommentFilter.java
+++ filterdemo/src/filter/CommentFilter.java
@@ -8,27 +8,44 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class CommentFilter implements Filter {
+	
+	private String[] illegalWords = new String[] {"fuck", "damn", "ÄãÂèµÄ"};
 
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void destroy() {}
 
 	@Override
-	public void doFilter(ServletRequest reqeust, ServletResponse response,
+	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		
-		System.out.println("doFilter");
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		resp.setContentType("text/html;charset=utf-8");
+		req.setCharacterEncoding("utf-8");
+		
+		String illegal = null;
+		String comment = req.getParameter("comment");
+		for (String word : illegalWords) {
+			if (comment.indexOf(word) != -1) {
+				illegal = word;
+				break;
+			}
+		}
+		
+		if (illegal != null)
+			resp.getWriter().print(String.format("<h2>Ãô¸Ð´Ê£º%s</h2>", illegal));
+		else
+			chain.doFilter(request, response);
 		
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void init(FilterConfig arg0) throws ServletException {}
+	
 }
