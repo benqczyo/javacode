e117cb7764b5bb7e3920543fc4bfc89a9a0ba54e
benqczyo <benqczyo@outlook.com>
2016-05-13 12:09:57 +0800

    update

---
+++ tag/src/com/benqcz/tag/Hide.java
@@ -0,0 +1,14 @@
+package com.benqcz.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class Hide extends TagSupport {
+	
+	@Override
+	public int doStartTag() throws JspException {
+		return Tag.SKIP_BODY;
+	}
+	
+}
