c786e5b8d3dd2ec16fe6af52f50bfc329f3d120e
benqczyo <benqczyo@outlook.com>
2016-06-23 13:42:50 +0800

    update

--- ikanke/src/com/benqcz/ikanke/domain/impl/CartImpl.java
+++ ikanke/src/com/benqcz/ikanke/domain/impl/CartImpl.java
@@ -2,21 +2,28 @@ package com.benqcz.ikanke.domain.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.benqcz.ikanke.domain.Cart;
 
 public class CartImpl implements Cart {
 	
 	private Map<String, CartItem> items = new HashMap<String, CartItem>();
-	private int numberOfItems;
+	private int numberOfBooks;
 	private float totalPrice;
 	
-	public int getNumberOfItems() {
-		return items.size();
+	public int getNumberOfBooks() {
+		int result = 0;
+		for (Entry<String, CartItem> item : items.entrySet())
+			result += item.getValue().getNumberOfBooks();
+		return result;
 	}
 
 	public float getTotalPrice() {
-		return totalPrice;
+		float result = 0;
+		for (Entry<String, CartItem> item : items.entrySet())
+			result += item.getValue().getTotalPrice();
+		return result;
 	}
 
 	public Map<String, CartItem> getItems() {
